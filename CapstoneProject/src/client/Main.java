b7bf2a111b38c61c086cf00ff27165e323504854
kazn00 <knakao935@student.fuhsd.org>
2022-05-01 16:50:09 -0700

    Update Main.java

--- CapstoneProject/src/client/Main.java
+++ CapstoneProject/src/client/Main.java
@@ -1,9 +1,18 @@
 package client;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("carrying");
+		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+	    root.setLevel(ch.qos.logback.classic.Level.ERROR);
+	    
+	    String[] options = { "Student", "Teacher" };
+	    
+	    int demo = JOptionPane.showOptionDialog(null, "Are you a STUDENT or TEACHER", 
+	    		"Networking Demo", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+		
 	}
 
 }
