ece54b3e16d77ed9404680fd739c1d9c306f3982
Kamran Hussain <kamran.hssn05@gmail.com>
2022-05-03 19:54:08 -0700

    Update StringChecker.java

--- CapstoneProject/src/data/StringChecker.java
+++ CapstoneProject/src/data/StringChecker.java
@@ -3,6 +3,6 @@ package data;
 public class StringChecker {
 
 	public static boolean isEmpty(String input) {
-		return input == null || input.isEmpty() || input.isBlank();
+		return input == null || input.isEmpty();
 	}
 }
