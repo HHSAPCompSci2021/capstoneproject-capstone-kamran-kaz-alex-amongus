5b86a5562c0f56c263bb07a0e9ab12ef493ac8b1
Kamran Hussain <kamran.hssn05@gmail.com>
2022-05-06 13:04:32 -0700

    Update ModelLoader.java

--- CapstoneProject/src/ServerClient/ModelLoader.java
+++ CapstoneProject/src/ServerClient/ModelLoader.java
@@ -4,7 +4,28 @@ public class ModelLoader {
 	
 	private String path = "";
 	
-	public ModelLoader() {
+	/**
+	 * loads the model from the specified path
+	 * @param path The path to the directory containing the model
+	 */
+	public ModelLoader(String ModelPath) {
+		path = ModelPath;
 		
+	}
+
+	/**
+	 * 
+	 * @return The path to the directory containing the model
+	 */
+	public String getPath() {
+		return path;
+	}
+
+	/**
+	 * 
+	 * @param path The path to the model's directory 
+	 */
+	public void setPath(String path) {
+		this.path = path;
 	}
 }
