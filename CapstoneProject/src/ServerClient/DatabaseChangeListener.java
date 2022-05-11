1f0297ff5c7c701027600dc4aa66628c71b1ff2f
alexwang05 <hpwzz20@gmail.com>
2022-05-10 21:58:12 -0700

    back from covid quarantine

---
+++ CapstoneProject/src/ServerClient/DatabaseChangeListener.java
@@ -0,0 +1,78 @@
+package ServerClient;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
+/**
+ * Handles changes with Firebase database reference
+ * @author Alex Wang, John Shelby for assistance in Firebase logic
+ *
+ */
+public class DatabaseChangeListener implements ChildEventListener {
+	private ConcurrentLinkedQueue<Runnable> tasks;
+	
+	public DatabaseChangeListener() {   // This threading strategy will work with Processing programs. Just use this code inside your PApplet.
+		tasks = new ConcurrentLinkedQueue<Runnable>();
+	}
+	
+	
+	public void post() {
+		while (!tasks.isEmpty()) {
+			Runnable r = tasks.remove();
+			r.run();
+		}
+	}
+	
+	
+	@Override
+	public void onCancelled(DatabaseError arg0) {
+
+	}
+
+	@Override
+	/**
+	 * This method is called automatically every time something is added to the database (by you or
+	 * someone else). It is also called at the beginning of the program for all existing database posts. 
+	 */
+	public void onChildAdded(DataSnapshot dataSnapshot, String arg1) {
+//		tasks.add(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				Post post = dataSnapshot.getValue(Post.class);
+//				currentDrawing.addDotSet(post.dots, new Color(post.r,post.g,post.b));
+//			}
+//			
+//		});
+		
+		// get value of stored information and turn back to Java object
+		System.out.println("onChildAdded() called");
+	}
+
+	@Override
+	public void onChildChanged(DataSnapshot arg0, String arg1) {
+		System.out.println("onChildChanged() called");
+	}
+
+	@Override
+	public void onChildMoved(DataSnapshot arg0, String arg1) {
+		System.out.println("onChildMoved() called");
+	}
+
+	@Override
+	public void onChildRemoved(DataSnapshot arg0) {
+		System.out.println("onChildRemoved() called");
+//		tasks.add(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				currentDrawing.clear();
+//			}
+//			
+//		});
+		
+	}
+}
