29811bd798d9c0c40349fe11da2a5a3cd65962a6
alexwang05 <hpwzz20@gmail.com>
2022-05-15 23:38:05 -0700

    initial ServerGui

--- CapstoneProject/src/ServerClient/ServerGui.java
+++ CapstoneProject/src/ServerClient/ServerGui.java
@@ -1,19 +1,70 @@
 package ServerClient;
 
 import java.awt.Color;
+import java.util.Scanner;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
+/**
+ * Screen to visualize Server task progress
+ * @author Alex wang
+ *
+ */
 public class ServerGui  {
+	private JProgressBar taskProgressBar;
+	private int progressVal;
+	
 	public static void main(String[] args) {
+		ServerGui serverGui = new ServerGui();
+		
+		
+		// testing code
+		Scanner in = new Scanner(System.in);
+		
+		while (true) {
+			System.out.println("Enter ProgressVal: ");
+			serverGui.setProgressVal(in.nextInt());
+		}
+	}
+	
+	public ServerGui() {
 		JFrame frame = new JFrame("GradeMe Server");
 	    frame.setBounds(100, 100, 1200, 650);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setBackground(new Color(211,211,211));
+
+		// current task title
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		
+		// current task progress percentage & progress bar
+		JPanel progressRow = new JPanel();
+		progressRow.setLayout(new BoxLayout(progressRow, BoxLayout.LINE_AXIS));
+		
+		progressVal = 0; // dummy data
+		JLabel progressLabel = new JLabel(String.format("Task Progress: %d", progressVal));
+		
+		taskProgressBar = new JProgressBar(0, 100); // modify max val as needed
+		taskProgressBar.setValue(progressVal);
+		taskProgressBar.setStringPainted(true);
+		
+		progressRow.add(progressLabel);
+		progressRow.add(taskProgressBar);
+		
+		panel.add(progressRow);
+		
+		// loss value label
+		
+		
+		frame.add(panel);
 		frame.setVisible(true);
-		JLabel label = new JLabel("Database Connected");
-		label.setBounds(50,100,20,70);
-		frame.add(label);
+	}
+	
+	public void setProgressVal(int progressVal) {
+		this.progressVal = progressVal;
 	}
 }
