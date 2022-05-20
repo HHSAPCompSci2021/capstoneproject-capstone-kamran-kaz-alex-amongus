e86706b218d2808ef04f4984ac616ff150416868
Kamran Hussain <kamran.hssn05@gmail.com>
2022-05-20 13:38:15 -0700

    Update dependencies

--- CapstoneProject/src/testing/ModelTester.java
+++ CapstoneProject/src/testing/ModelTester.java
@@ -8,12 +8,14 @@ public class ModelTester {
 
 	public static void main(String[] args) {
 		try {
-			Scanner kboard = new Scanner(System.in);
-			System.out.println("Enter first phrase: ");
-			String in1 = kboard.nextLine();
-			System.out.println("Enter second phrase: ");
-			String in2 = kboard.nextLine();
-			new BertSemanticGraderModel(false).predict(in1, in2);
+//			Scanner kboard = new Scanner(System.in);
+//			System.out.println("Enter first phrase: ");
+//			String in1 = kboard.nextLine();
+//			System.out.println("Enter second phrase: ");
+//			String in2 = kboard.nextLine();
+//			new BertSemanticGraderModel(false).predict(in1, in2);
+			
+			new BertSemanticGraderModel(false).predict("hello world", "hi world");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
